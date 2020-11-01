083fdfca73012d6858aa7655ee96e2a069b4a3ab
Aleyna Güner <aleynaguner@hotmail.com>
2020-11-01 18:58:32 +0300

    Loops

--- Loops/nestedLoops.c
+++ Loops/nestedLoops.c
@@ -2,6 +2,18 @@
 
 int main()
 {
+    int i, j;
+
+    for (i = 2; i < 100; i++)
+    {
+        for (j = 2; j <= (i/j); j++)
+        {
+            if (i % j == 0)
+                break;
+        }
+        if(j > (i/j))
+            printf("%d is prime number\n", i);
+    }
 
     return 0;
 }
--- Loops/nestedLoops1.c
+++ Loops/nestedLoops1.c
@@ -2,6 +2,16 @@
 
 int main()
 {
+    int i, j;
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            printf("%d\t", i + j);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
